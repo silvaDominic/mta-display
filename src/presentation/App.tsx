1f46d8e4004cb6b0de3f7c9ce30e751e83744997
Dominic <string.reverse@proton.me>
2024-10-18 15:11:35 -0400

    Added directional arrows and converted App styles to scss

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,20 +1,29 @@
-import './App.css'
+import './App.scss'
 import { Card } from "./components/card.tsx";
 
 function App() {
 
   return (
     <>
-      <Card
-        title='Forest Hills'
-        trainLine='R'
-        minute={2}
-      />
-      <Card
-        title='96St'
-        trainLine='Q'
-        minute={1}
-      />
+      <div id="deck">
+        <div className="left-direction direction">
+          <img src="./public/icons/arrow-left-circle-outlined.svg" alt="Left arrow"/>
+          <Card
+            title='Forest Hills'
+            trainLine='R'
+            minute={2}
+          />
+        </div>
+
+        <div className="right-direction direction">
+          <img src="./public/icons/arrow-right-circle-outlined.svg" alt="Right arrow"/>
+          <Card
+            title='96St'
+            trainLine='Q'
+            minute={1}
+          />
+        </div>
+      </div>
     </>
   )
 }
