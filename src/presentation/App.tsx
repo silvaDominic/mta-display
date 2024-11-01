61f21b93acadd56b3fe1b45362854e34fb8f8703
Dominic <string.reverse@proton.me>
2024-11-01 16:20:25 -0400

    Added 'stack' of cards and adjused CSS to cater to layered design

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -5,23 +5,63 @@ function App() {
 
   return (
     <>
-      <div id="deck">
-        <div className="left-direction direction">
-          <img src="./public/icons/arrow-left-circle-outlined.svg" alt="Left arrow"/>
-          <Card
-            title='Forest Hills'
-            trainLine='R'
-            minute={2}
-          />
-        </div>
+      <div id="stack">
+        <div className="container first">
+          <div className="left-direction direction">
+            <img src="./public/icons/arrow-left-circle-outlined.svg" alt="Left arrow"/>
+            <Card
+              title='Forest Hills'
+              trainLine='R'
+              minute={2}
+            />
+          </div>
 
-        <div className="right-direction direction">
-          <img src="./public/icons/arrow-right-circle-outlined.svg" alt="Right arrow"/>
-          <Card
-            title='96St'
-            trainLine='Q'
-            minute={1}
-          />
+          <div className="right-direction direction">
+            <img src="./public/icons/arrow-right-circle-outlined.svg" alt="Right arrow"/>
+            <Card
+              title='96St'
+              trainLine='Q'
+              minute={1}
+            />
+          </div>
+        </div>
+        <div className="container second">
+          <div className="left-direction direction">
+            <img src="./public/icons/arrow-left-circle-outlined.svg" alt="Left arrow"/>
+            <Card
+              title='Forest Hills'
+              trainLine='R'
+              minute={2}
+            />
+          </div>
+
+          <div className="right-direction direction">
+            <img src="./public/icons/arrow-right-circle-outlined.svg" alt="Right arrow"/>
+            <Card
+              title='96St'
+              trainLine='Q'
+              minute={1}
+            />
+          </div>
+        </div>
+        <div className="container third">
+          <div className="left-direction direction">
+            <img src="./public/icons/arrow-left-circle-outlined.svg" alt="Left arrow"/>
+            <Card
+              title='Forest Hills'
+              trainLine='R'
+              minute={2}
+            />
+          </div>
+
+          <div className="right-direction direction">
+            <img src="./public/icons/arrow-right-circle-outlined.svg" alt="Right arrow"/>
+            <Card
+              title='96St'
+              trainLine='Q'
+              minute={1}
+            />
+          </div>
         </div>
       </div>
     </>
