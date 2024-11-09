a6af0738538b1e5541b9f331591a0089093576ee
Dominic <string.reverse@proton.me>
2024-11-08 19:25:33 -0500

    Added 'Single Platform Display' and adjusted styling
    - Added selector for single/multi platform display
    - Adjusted styles

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -4,9 +4,16 @@ import { ArrivalInfoModel } from "../application/models/arrival-info.model.ts";
 import { RouteService } from "../application/services/route.service.ts";
 import { DIRECTION } from "../shared/constants/direction.enum.ts";
 import { MultiPlatformDisplay } from "./features/multi-platform/multi-platform-display.feature";
+import { SinglePlatformDisplay } from "./features/single-platform/single-platform-display.feature";
+
+enum DisplayType {
+  Single,
+  Multi,
+}
 
 function App() {
   const [arrivalTimes, setArrivalTimes] = useState<Map<DIRECTION, ArrivalInfoModel[]>>(new Map<DIRECTION, ArrivalInfoModel[]>());
+  const [displayType, setDisplayType] = useState<DisplayType>(DisplayType.Single);
 
   useEffect(() => {
     const map = new Map<DIRECTION, ArrivalInfoModel[]>();
@@ -21,6 +28,31 @@ function App() {
     });
   }, []);
 
-  return <MultiPlatformDisplay arrivalTimes={arrivalTimes}/>;
+  function renderDisplay() {
+    switch(displayType) {
+      case DisplayType.Single:
+        return <SinglePlatformDisplay arrivalTimes={arrivalTimes} direction={DIRECTION.N}/>;
+      case DisplayType.Multi:
+        return <MultiPlatformDisplay arrivalTimes={arrivalTimes}/>;
+    }
+  }
+
+  return (
+    <>
+      <div>
+        <select id="display-select" onChange={(e) => setDisplayType(Number(e.target.value))}>
+          <option value={DisplayType.Single}>Single-platform</option>
+          <option value={DisplayType.Multi}>Multi-platform</option>
+        </select>
+      </div>
+
+      {renderDisplay()}
+    </>
+
+
+
+)
+  ;
 }
+
 export default App;
---
+++ src/presentation/features/single-platform/single-platform-display.feature.tsx
@@ -0,0 +1,29 @@
+import { ReactElement } from "react";
+import { DIRECTION } from "../../../shared/constants/direction.enum";
+import { ArrivalInfoModel } from "../../../application/models/arrival-info.model";
+import { Card } from "../../components/card";
+import './single-platform-display.styles.scss';
+
+type SinglePlatformDisplayProps = {
+  arrivalTimes: Map<DIRECTION, ArrivalInfoModel[]>,
+  direction: DIRECTION,
+}
+
+export function SinglePlatformDisplay({arrivalTimes, direction}: SinglePlatformDisplayProps): ReactElement {
+  return (
+    <div className="spd container">
+        {
+          arrivalTimes?.get(DIRECTION[direction])?.map((arrData: ArrivalInfoModel, index: number) => (
+            <div className={`spd wrapper pos-${index}`}>
+                <Card
+                  title={arrData.destination}
+                  trainLine={arrData.line.toString()}
+                  minute={arrData.getArrivalTimeInMinutes()}
+                  style={{ height: '200px' }}
+                />
+            </div>
+          ))
+        }
+      </div>
+  );
+}
