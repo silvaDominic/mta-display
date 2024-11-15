f31ad7c62b5e4897811e7c81b335fb3f0f2cfff1
Dominic <string.reverse@proton.me>
2024-11-15 16:42:44 -0500

    Removed strict mode

--- src/presentation/main.tsx
+++ src/presentation/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app.tsx'
 import './index.css'
@@ -8,9 +7,7 @@ const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
   root.render(
-    <StrictMode>
       <App />
-    </StrictMode>
   );
 } else {
   console.error('Root element not found');
