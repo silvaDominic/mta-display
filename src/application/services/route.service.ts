883abd1c70fa9c297f665849f617b2570fdc6c72
Dominic <string.reverse@proton.me>
2024-11-15 12:12:16 -0500

    Altered 'route service'
    RouteService now takes a direction and returns a filtered list based on direciton

--- src/application/services/route.service.ts
+++ src/application/services/route.service.ts
@@ -7,17 +7,24 @@ function fakeId(): string{
 }
 
 const STUB_ROUTE_INFO = [
-  new ArrivalInfoModel(fakeId(), TRAIN_LINE.G, Date.now(), 'Church Ave', DIRECTION.S),
-  new ArrivalInfoModel(fakeId(), TRAIN_LINE.G, Date.now(), 'Court St', DIRECTION.N),
-  new ArrivalInfoModel(fakeId(), TRAIN_LINE.G, Date.now(), 'Court St', DIRECTION.N),
-  new ArrivalInfoModel(fakeId(), TRAIN_LINE.G, Date.now(), 'Church Ave', DIRECTION.S),
-  new ArrivalInfoModel(fakeId(), TRAIN_LINE.G, Date.now(), 'Church Ave', DIRECTION.S),
-  new ArrivalInfoModel(fakeId(), TRAIN_LINE.G, Date.now(), 'Court St', DIRECTION.N),
+  new ArrivalInfoModel(fakeId(), TRAIN_LINE.G, Date.now(), Date.now() + 10, 'Church Ave', DIRECTION.S),
+  new ArrivalInfoModel(fakeId(), TRAIN_LINE.G, Date.now(), Date.now() + 10, 'Court St', DIRECTION.N),
+  new ArrivalInfoModel(fakeId(), TRAIN_LINE.G, Date.now(), Date.now() + 10, 'Court St', DIRECTION.N),
+  new ArrivalInfoModel(fakeId(), TRAIN_LINE.G, Date.now(), Date.now() + 10, 'Church Ave', DIRECTION.S),
+  new ArrivalInfoModel(fakeId(), TRAIN_LINE.G, Date.now(), Date.now() + 10, 'Church Ave', DIRECTION.S),
+  new ArrivalInfoModel(fakeId(), TRAIN_LINE.G, Date.now(), Date.now() + 10, 'Court St', DIRECTION.N),
 ];
 
 export const RouteService = {
-  getArrivalTimes(platformId: string): Promise<ArrivalInfoModel[]> {
+  getArrivalTimes(platformId: string, direction: DIRECTION): Promise<ArrivalInfoModel[]> {
     console.log(`Times for ${platformId}`);
-    return Promise.resolve(STUB_ROUTE_INFO);
+    switch(direction) {
+      case DIRECTION.N:
+        return Promise.resolve(STUB_ROUTE_INFO).then(res => res.filter((data: ArrivalInfoModel)=> data.direction === DIRECTION.N));
+      case DIRECTION.S:
+        return Promise.resolve(STUB_ROUTE_INFO).then(res => res.filter((data: ArrivalInfoModel)=> data.direction === DIRECTION.S));
+      case DIRECTION.BOTH:
+        return Promise.resolve(STUB_ROUTE_INFO);
+    }
   }
 }
--- src/shared/constants/direction.enum.ts
+++ src/shared/constants/direction.enum.ts
@@ -1,4 +1,5 @@
 export enum DIRECTION {
   N = "N",
   S = "S",
+  BOTH = "BOTH",
 }
