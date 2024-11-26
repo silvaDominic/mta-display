08201f045c0c4fe792f6e427e3d503b5d368f7c1
Dominic <string.reverse@proton.me>
2024-11-26 15:20:34 -0500

    Improved API and render method of 'Multi-Direction Display'

--- src/presentation/features/multi-platform/multi-direction-display.feature.tsx
+++ src/presentation/features/multi-platform/multi-direction-display.feature.tsx
@@ -1,19 +1,24 @@
-import { DIRECTION } from "../../../shared/constants/direction.enum";
 import { ArrivalInfoModel } from "../../../application/models/arrival-info.model";
 import { Card } from "../../components/card";
 import './multi-direction-display.styles.scss';
 
 type MultiDirectionDisplayProps = {
-  arrivalTimes: Map<DIRECTION, ArrivalInfoModel[]>;
+  leftSideArrivals: ArrivalInfoModel[];
+  rightSideArrivals: ArrivalInfoModel[];
 }
 
-export function MultiDirectionDisplay({arrivalTimes}: MultiDirectionDisplayProps) {
+export function MultiDirectionDisplay({leftSideArrivals, rightSideArrivals}: MultiDirectionDisplayProps) {
+  const sortedLeftSideArrivals = leftSideArrivals
+    .sort((timeA, timeB) => timeB.getArrivalTimeInMinutes() - timeA.getArrivalTimeInMinutes()).slice(0, 3);
+  const sortedRightSideArrivals = rightSideArrivals
+    .sort((timeA, timeB) => timeB.getArrivalTimeInMinutes() - timeA.getArrivalTimeInMinutes()).slice(0, 3);
+
   return (
     <>
       <div id="stack">
         <div className="mdd container">
           {
-            arrivalTimes?.get(DIRECTION.N)?.map((arrData: ArrivalInfoModel, index: number) => (
+            sortedLeftSideArrivals.map((arrData: ArrivalInfoModel, index: number) => (
               <div key={arrData.id} className={`mdd wrapper pos-${index}`}>
                 <div className="left-direction direction">
                   <img src="../../public/icons/arrow-left-circle-outlined.svg" alt="Left arrow"/>
@@ -31,7 +36,7 @@ export function MultiDirectionDisplay({arrivalTimes}: MultiDirectionDisplayProps
 
         <div className="mdd container">
           {
-            arrivalTimes?.get(DIRECTION.S)?.map((arrData: ArrivalInfoModel, index: number) => (
+            sortedRightSideArrivals.map((arrData: ArrivalInfoModel, index: number) => (
               <div key={arrData.id} className={`mdd wrapper pos-${index}`}>
                 <div className="right-direction direction">
                   <img src="../../public/icons/arrow-right-circle-outlined.svg" alt="Right arrow"/>
