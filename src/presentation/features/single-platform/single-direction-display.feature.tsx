96945ae4f965497501a53eefefa67c490caf89d1
Dominic <string.reverse@proton.me>
2024-12-02 17:01:41 -0500

    Added support for alerts in 'Single Direction Display' component

--- src/presentation/features/single-platform/single-direction-display.feature.tsx
+++ src/presentation/features/single-platform/single-direction-display.feature.tsx
@@ -3,15 +3,18 @@ import { ArrivalInfoModel } from "../../../application/models/arrival-info.model
 import { Card } from "../../components/card";
 import './single-direction-display.styles.scss';
 import { Debug } from "../../components/debug";
+import { AlertModel } from "../../../application/models/alert.model";
 
 type SingleDirectionDisplayProps = {
   arrivalTimes: ArrivalInfoModel[],
+  alerts: AlertModel[],
 }
 
-export function SingleDirectionDisplay({arrivalTimes}: SingleDirectionDisplayProps): ReactElement {
+export function SingleDirectionDisplay({arrivalTimes, alerts}: SingleDirectionDisplayProps): ReactElement {
   const sortedTimes: ArrivalInfoModel[] = arrivalTimes.sort((timeA, timeB) => timeB.getTimeUntilArrivalInMinutes() - timeA.getTimeUntilArrivalInMinutes());
   const [displayedTimes, setDisplayedTimes] = useState<ArrivalInfoModel[]>(sortedTimes);
   const [isDeleting, setIsDeleting] = useState<boolean>(false);
+  const [isShowingAlert, setIsShowingAlert] = useState<boolean>(false);
 
   useEffect(() => {
     setDisplayedTimes((prev: ArrivalInfoModel[]) => {
@@ -23,19 +26,39 @@ export function SingleDirectionDisplay({arrivalTimes}: SingleDirectionDisplayPro
     });
   }, [sortedTimes]);
 
+  useEffect(() => {
+    setIsShowingAlert(alerts.length > 0);
+  }, [alerts]);
+
   function onTransitionEnd() {
     setIsDeleting(false);
     setDisplayedTimes(sortedTimes);
   }
 
+  function applyDeleteClasses(index: number): string {
+    return `${index === 2 && isDeleting ? 'deleting' : ''}`
+  }
+
+  function applyAlertClasses(): string {
+    return isShowingAlert ? 'alerting' : '';
+  }
+
+  function applyBoardingClass(arrivalInfo: ArrivalInfoModel): string {
+    return '';
+    return `${arrivalInfo.getTimeUntilDepartureInMinutes() === 0 ? 'boarding' : ''}`;
+  }
+
   return (
     <>
-      <div className="sdd container">
+      <div className={`sdd container ${applyAlertClasses()}`}>
         {
           displayedTimes?.map((arrData: ArrivalInfoModel, index: number) => (
             <div
               key={arrData.id}
-              className={`sdd wrapper pos-${index} ${index === 2 && isDeleting ? 'deleting' : ''}`}
+              className={
+              `sdd wrapper pos-${index}
+              ${applyDeleteClasses(index)}`
+            }
               onTransitionEnd={onTransitionEnd}
             >
               <Card
@@ -43,10 +66,18 @@ export function SingleDirectionDisplay({arrivalTimes}: SingleDirectionDisplayPro
                 trainLine={arrData.line.toString()}
                 minute={arrData.getTimeUntilArrivalInMinutes()}
                 isFront={index > 1}
-                className={`${arrData.getTimeUntilDepartureInMinutes() === 0 ? 'boarding' : ''}`}
+                className={applyBoardingClass(arrData)}
               />
             </div>
           ))
+        }
+        {
+          alerts.length > 0 && (
+            <div className='alert-container'>
+              <div className='alert-description'>{alerts[0].description}</div>
+              <div className='alert-resolution'>{alerts[0].resolution}</div>
+            </div>
+          )
         }
       </div>
 
