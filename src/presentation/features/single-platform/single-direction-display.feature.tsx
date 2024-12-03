46d262fdb4e445c59dc9b1eb04ba9b0b34d37cd6
Dominic <string.reverse@proton.me>
2024-12-03 13:58:24 -0500

    Made accommodations for new 'Alert' comp in 'Single Display Direction' comp

--- src/presentation/features/single-platform/single-direction-display.feature.tsx
+++ src/presentation/features/single-platform/single-direction-display.feature.tsx
@@ -1,11 +1,10 @@
-import { ReactElement, useEffect, useRef, useState } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import { ArrivalInfoModel } from "../../../application/models/arrival-info.model";
 import { Card } from "../../components/card";
 import './single-direction-display.styles.scss';
 import { Debug } from "../../components/debug";
 import { AlertModel } from "../../../application/models/alert.model";
-
-const BOTTOM_PADDING_OFFSET = 25;
+import { Alert } from "../../components/alert";
 
 type SingleDirectionDisplayProps = {
   arrivalTimes: ArrivalInfoModel[],
@@ -18,8 +17,6 @@ export function SingleDirectionDisplay({arrivalTimes, alerts, onAlertEnd}: Singl
   const [displayedTimes, setDisplayedTimes] = useState<ArrivalInfoModel[]>(sortedTimes);
   const [isDeleting, setIsDeleting] = useState<boolean>(false);
   const [isShowingAlert, setIsShowingAlert] = useState<boolean>(false);
-  const alertContainerRef = useRef<HTMLDivElement>(null);
-  const alertResolutionRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     setDisplayedTimes((prev: ArrivalInfoModel[]) => {
@@ -34,38 +31,6 @@ export function SingleDirectionDisplay({arrivalTimes, alerts, onAlertEnd}: Singl
   useEffect(() => {
     setIsShowingAlert(alerts.length > 0);
   }, [alerts]);
-
-  /**
-   * Handles scrolling alert content from bottom to top.
-   * Based on a scroll increment and interval frequency, a scroll function is executed that
-   * increments a reference to the top of the scrollable container until it reaches a limit.
-   */
-  useEffect(() => {
-    const alertContainer = alertContainerRef.current;
-    const alertResolution = alertResolutionRef.current;
-
-    if (!alertContainer || !alertResolution) return;
-
-    // Presets
-    const offset = parseInt((window.getComputedStyle(alertResolution).paddingBottom)) - BOTTOM_PADDING_OFFSET;
-    const scrollIncrement = 1;
-    alertContainer.scrollTop = 0;
-
-    const intervalId = setInterval(scroll, 20);
-
-    function scroll(): void {
-      if (alertContainer.scrollTop < alertContainer.scrollHeight - offset) {
-        alertContainer.scrollTop += scrollIncrement;
-      } else {
-        // TODO: Consider how this will be handled with real time data
-        setIsShowingAlert(false);
-        onAlertEnd();
-        clearInterval(intervalId);
-      }
-    }
-
-    return () => clearInterval(intervalId);
-  }, [isShowingAlert, onAlertEnd]);
 
   function onTransitionEnd() {
     setIsDeleting(false);
@@ -108,14 +73,7 @@ export function SingleDirectionDisplay({arrivalTimes, alerts, onAlertEnd}: Singl
             </div>
           ))
         }
-        {
-          alerts.length > 0 && (
-            <div ref={alertContainerRef} className='alert-container'>
-              <div className='alert-description'>{alerts[0].description}</div>
-              <div ref={alertResolutionRef} className='alert-resolution'>{alerts[0].resolution}</div>
-            </div>
-          )
-        }
+        { alerts.length > 0 && <Alert alert={alerts[0]} onAlertEnd={onAlertEnd} /> }
       </div>
 
       <hr/>
