b3a33ca15722e091316ec62c75ca995b3d8920fe
Dominic <string.reverse@proton.me>
2024-12-10 14:20:33 -0500

    Added train lines constant
    - Added new constants files in presentation layer

---
+++ src/presentation/constants.ts
@@ -0,0 +1,142 @@
+export const TRAIN_LINES = [
+  {
+    id: '1',
+    src: '/train-line-icons/1.svg',
+    alt: '1'
+  },
+  {
+    id: '2',
+    src: '/train-line-icons/2.svg',
+    alt: '2'
+  },
+  {
+    id: '3',
+    src: '/train-line-icons/3.svg',
+    alt: '3'
+  },
+  {
+    id: '4',
+    src: '/train-line-icons/4.svg',
+    alt: '4'
+  },
+  {
+    id: '5',
+    src: '/train-line-icons/5.svg',
+    alt: '5'
+  },
+  {
+    id: '6',
+    src: '/train-line-icons/6.svg',
+    alt: '6'
+  },
+  {
+    id: '6d',
+    src: '/train-line-icons/6d.svg',
+    alt: '6d'
+  },
+  {
+    id: '7',
+    src: '/train-line-icons/7.svg',
+    alt: '7'
+  },
+  {
+    id: '7d',
+    src: '/train-line-icons/7d.svg',
+    alt: '7d'
+  },
+  {
+    id: 'a',
+    src: '/train-line-icons/a.svg',
+    alt: 'A'
+  },
+  {
+    id: 'c',
+    src: '/train-line-icons/c.svg',
+    alt: 'C'
+  },
+  {
+    id: 'e',
+    src: '/train-line-icons/e.svg',
+    alt: 'E'
+  },
+  {
+    id: 'b',
+    src: '/train-line-icons/b.svg',
+    alt: 'B'
+  },
+  {
+    id: 'd',
+    src: '/train-line-icons/d.svg',
+    alt: 'D'
+  },
+  {
+    id: 'f',
+    src: '/train-line-icons/f.svg',
+    alt: 'F'
+  },
+  {
+    id: 'm',
+    src: '/train-line-icons/m.svg',
+    alt: 'M'
+  },
+  {
+    id: 'g',
+    src: '/train-line-icons/g.svg',
+    alt: 'G'
+  },
+  {
+    id: 'l',
+    src: '/train-line-icons/l.svg',
+    alt: 'L'
+  },
+  {
+    id: 'j',
+    src: '/train-line-icons/j.svg',
+    alt: 'J'
+  },
+  {
+    id: 'z',
+    src: '/train-line-icons/z.svg',
+    alt: 'Z'
+  },
+  {
+    id: 'n',
+    src: '/train-line-icons/n.svg',
+    alt: 'N'
+  },
+  {
+    id: 'q',
+    src: '/train-line-icons/q.svg',
+    alt: 'Q'
+  },
+  {
+    id: 'r',
+    src: '/train-line-icons/r.svg',
+    alt: 'R'
+  },
+  {
+    id: 'w',
+    src: '/train-line-icons/w.svg',
+    alt: 'W'
+  },
+  {
+    id: 's',
+    src: '/train-line-icons/s.svg',
+    alt: 'S'
+  },
+  {
+    id: 'sf',
+    src: '/train-line-icons/sf.svg',
+    alt: 'S,F'
+  },
+  {
+    id: 'sir',
+    src: '/train-line-icons/sir.svg',
+    alt: 'S,I,R'
+  },
+  {
+    id: 'sr',
+    src: '/train-line-icons/sr.svg',
+    alt: 'S,R'
+  },
+];
