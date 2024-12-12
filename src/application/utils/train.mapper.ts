387cc2d78c43a40f5ce63d40880fa7713dd6e603
Dominic <string.reverse@proton.me>
2024-12-12 12:28:53 -0500

    Added 'dto to stop info model' mapper function

--- src/application/utils/train.mapper.ts
+++ src/application/utils/train.mapper.ts
@@ -1,7 +1,21 @@
 import { StationModel } from "../models/station.model";
+import { StopInfoModel } from "../models/stop-info.model";
 
 export const TrainMapper = {
   dtoToStationModel(dto: any): StationModel[] {
     return dto['serviceMaps'][0]['stops'].map(item => new StationModel(item.id, item.name));
   },
+
+  dtoToStopInfoModel(dto: any, direction): StopInfoModel[] {
+    return dto.stopTimes.map((info: any) => (
+      new StopInfoModel(
+        info.arrival.time,
+        info.trip.route.id,
+        info.arrival.time,
+        info.departure.time,
+        info.trip.destination.name,
+        direction
+      )
+    ));
+  }
 }
