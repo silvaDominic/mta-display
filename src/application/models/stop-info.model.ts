6ed40a1d95ade605f9fd454588b3c274b837c2c0
Dominic <string.reverse@proton.me>
2024-12-17 12:57:17 -0500

    Corrected utc to minutes helper functions in 'stop info' model

--- src/application/models/stop-info.model.ts
+++ src/application/models/stop-info.model.ts
@@ -13,10 +13,10 @@ export class StopInfoModel {
   ) {}
 
   getTimeUntilArrivalInMinutes(): number {
-    return Math.round(((this.arrivalTime * 1000) - Date.now()) / (60 * 1000));
+    return Math.round((this.arrivalTime - Date.now() / 1000) / 60);
   }
 
   getTimeUntilDepartureInMinutes(): number {
-    return Math.round(((this.departureTime * 1000) - Date.now()) / (60 * 1000));
+    return Math.round((this.departureTime - Date.now() / 1000) / 60);
   }
 }
